df4075093ea889fb40551f8361437335ed2b9d00
Godana Emiru <godanaemiru@gmail.com>
2026-01-02 21:30:29 +0300

    answered 1st question

---
+++ kgl_validation.js
@@ -0,0 +1,18 @@
+const companyName = "Karibu Groceries LTD";
+// I used const because the company name is a fixed value that should not change.
+const minimumTonnage = 1000;
+/* 
+I used const for minimum tonnage because it is a threshold value 
+representing a minimum value which is unlikely to change 
+*/
+let isOperational = true;
+
+let managerName;
+
+let closedBranches = null;
+
+console.log("Type of companyName:", typeof companyName);     
+console.log("Type of minimumTonnage:", typeof minimumTonnage);
+console.log("Type of isOperational:", typeof isOperational);   
+console.log("Type of managerName:", typeof managerName);       
+console.log("Type of closedBranches:", typeof closedBranches); 
