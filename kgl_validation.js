e50fd43640e3317bbf8b34064bd053e40046943c
Godana Emiru <godanaemiru@gmail.com>
2026-01-02 21:32:41 +0300

    added more comments

--- kgl_validation.js
+++ kgl_validation.js
@@ -6,11 +6,17 @@ I used const for minimum tonnage because it is a threshold value
 representing a minimum value which is unlikely to change 
 */
 let isOperational = true;
-
+/* We use 'let' for isOperational because the operational status 
+   of a business can change (e.g., during maintenance or holidays).
+*/
 let managerName;
-
+/* We use 'let' for managerName because we are declaring it without an 
+   initial value, intending to assign it later. 'const' requires immediate assignment.
+*/
 let closedBranches = null;
-
+/* We use 'let' for closedBranches. While it is currently null, 
+   this value would likely be updated in case of a change in branch status.
+*/
 console.log("Type of companyName:", typeof companyName);     
 console.log("Type of minimumTonnage:", typeof minimumTonnage);
 console.log("Type of isOperational:", typeof isOperational);   
